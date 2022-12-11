6cfb671a54d66aee5054f833407dd72507422845
JavidDitty <92059735+JavidDitty@users.noreply.github.com>
2022-12-11 18:02:07 -0500

    Delete accessibility-checklist directory

--- accessibility-checklist/src/index.js
+++
@@ -1,8 +0,0 @@
-window.onload = function() {
-    Question.read("../res/questions.json");
-    const check = new ChecklistHandler();
-
-    check.sendQuestions();
-    document.getElementById("submit-btn").addEventListener("click", (e) => check.submit(e.target.attributes.href.nodeValue));
-    document.getElementById("clear-btn").addEventListener("click", () => check.clear());
-}
--- accessibility-checklist/src/result.js
+++
@@ -1,9 +0,0 @@
-window.onload = function() {
-    Question.read("../res/questions.json");
-    const check = new ChecklistHandler();
-
-    let params = new URLSearchParams(window.location.search);
-    check.sendScore(params.get("score"));
-    let tips = params.get("tips");
-    if(tips !== "") check.sendTips(tips.split(","));
-}
